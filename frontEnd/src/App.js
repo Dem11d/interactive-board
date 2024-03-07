448f522560e1b8fb0f84ad6685478ab122393b24
Demyd Pavliuk <dem11d92@gmail.com>
2024-03-07 14:33:52 +0200

    resolve conflict

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import DrawerComponent from './drawer/drawer';
+import Sidebar from './sidebar/sidebar';
 
 function App() {
   return (
     <div className="App">
-
+      <Sidebar></Sidebar>
       <DrawerComponent onCanvasChange={console.log}></DrawerComponent>
-    
+   
     </div>
   );
 }
