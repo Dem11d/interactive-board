333c47be44716f910eb13ee2bc631223463bdba6
mgolebiewski <marek@vhive.ai>
2024-03-07 14:04:33 +0100

    listen to websocket

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -5,6 +5,28 @@ import Sidebar from './sidebar/sidebar';
 
 function App() {
   const [canvasJson, setCanvasJson] = useState(null);
+
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:8080');
+
+    ws.onopen = () => {
+      console.log('WebSocket Connected');
+    };
+
+    ws.onmessage = (event) => {
+      const data = event.data;
+      setCanvasJson(JSON.parse(data).canvas);
+    };
+
+    ws.onerror = (error) => {
+      console.error('WebSocket Error:', error);
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
   return (
     <div className="App">
       <Sidebar></Sidebar>
@@ -12,7 +34,6 @@ function App() {
             onCanvasChange={console.log}
             canvasJson={canvasJson}
         />
-
     </div>
   );
 }
